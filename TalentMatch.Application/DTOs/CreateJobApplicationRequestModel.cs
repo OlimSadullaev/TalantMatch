a80d3f5176e854ee38e9a78202b0c2e9f7daf9dd
OlimSadullaev <olimxonnutfullaevich@gmail.com>
2025-09-03 23:51:25 +0500

    CreateJobApplicationCmmand

--- TalentMatch.Application/DTOs/CreateJobApplicationRequestModel.cs
+++ TalentMatch.Application/DTOs/CreateJobApplicationRequestModel.cs
@@ -1,13 +1,23 @@
-﻿namespace TalentMatch.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalentMatch.Application.DTOs
 {
     public class CreateJobApplicationRequestModel
     {
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
         public string PhoneNumber { get; set; }
         public string LinkedIn { get; set; }
-        public string CV { get; set; }
+        public string CvFilePath { get; set; }
+
+        [Required]
         public Guid JobId { get; set; }
     }
 }
---
+++ TalentMatch.Application/UseCases/JobApplications/Command/CreateJobApplicationCommand.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RecruitAI.Domain.Entities;
+using TalentMatch.Application.Abstractions;
+using TalentMatch.Application.DTOs;
+
+namespace TalentMatch.Application.UseCases.JobApplications.Command
+{
+    public class CreateJobApplicationCommand : ICommand<Unit>
+    {
+        public CreateJobApplicationRequestModel jobApplication { get; set; }
+
+        public CreateJobApplicationCommand(CreateJobApplicationRequestModel jobApplication)
+        {
+            this.jobApplication = jobApplication;
+        }
+    }
+
+    public class CreateJobApplicationCommandHandler
+    : ICommandHandler<CreateJobApplicationCommand, Unit>
+    {
+        private readonly IApplicationDbContext context;
+
+        public CreateJobApplicationCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(CreateJobApplicationCommand request, CancellationToken cancellationToken)
+        {
+            var jobApplication = new JobApplication
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.jobApplication.FirstName,
+                LastName = request.jobApplication.LastName,
+                Email = request.jobApplication.Email,
+                PhoneNumber = request.jobApplication.PhoneNumber,
+                LinkedIn = request.jobApplication.LinkedIn,
+                CV = request.jobApplication.CvFilePath,
+                JobId = request.jobApplication.JobId
+            };
+
+            await context.JobApplications.AddAsync(jobApplication, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
