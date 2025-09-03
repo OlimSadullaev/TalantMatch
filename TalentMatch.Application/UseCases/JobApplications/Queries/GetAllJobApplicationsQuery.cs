4359e9d5a1312a2f01c2c04ea7b6e6070932f1c9
OlimSadullaev <olimxonnutfullaevich@gmail.com>
2025-09-04 00:00:59 +0500

    GetAllJobApplication Created

---
+++ TalentMatch.Application/UseCases/JobApplications/Queries/GetAllJobApplicationsQuery.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TalentMatch.Application.Abstractions;
+using TalentMatch.Application.DTOs;
+using TalentMatch.Application.UseCases.JobApplications.Command;
+
+namespace TalentMatch.Application.UseCases.JobApplications.Queries
+{
+    public class GetAllJobApplicationsQuery : IQuery<List<CreateJobApplicationRequestModel>>
+    {
+    }
+
+    public class DeleteJobApplicationCommandHandler
+        : ICommandHandler<DeleteJobApplicationCommand, Unit>
+    {
+        private readonly IApplicationDbContext context;
+
+        public DeleteJobApplicationCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteJobApplicationCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.JobApplications
+                .FirstOrDefaultAsync(j => j.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+                throw new KeyNotFoundException("Job application not found");
+
+            context.JobApplications.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
